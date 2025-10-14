408da8cc147cbd371eef35a574754dbd92736f86
xyrylleclaire-matiga <c2097-23@itmlyceumalabang.onmicrosoft.com>
2025-10-14 13:45:12 +0800

    ISupplierService
    Signed-off-by: xyrylleclaire-matiga <c2097-23@itmlyceumalabang.onmicrosoft.com>

--- ShoeShop.Services/Interfaces/ISupplierService.cs
+++ ShoeShop.Services/Interfaces/ISupplierService.cs
@@ -4,11 +4,8 @@ using System.Threading.Tasks;
 
 namespace ShoeShop.Services.Interfaces
 {
-    // The interface the PurchaseOrderController requires for supplier lookup
-    // ✅ FIXED: Pinalitan ang return type mula sa 'object' patungong 'SupplierDto'
     public interface ISupplierService
     {
-        // Ang tamang signature ay dapat tumanggap ng SupplierDto, hindi generic object.
         Task<IEnumerable<SupplierDto>> GetAllSuppliersAsync();
     }
 }

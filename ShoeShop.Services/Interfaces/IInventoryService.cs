72a746ed05c0d0035119ba713bc0c2b9c7f53e25
xyrylleclaire-matiga <c2097-23@itmlyceumalabang.onmicrosoft.com>
2025-10-14 13:42:57 +0800

    IInventoryService
    Signed-off-by: xyrylleclaire-matiga <c2097-23@itmlyceumalabang.onmicrosoft.com>

--- ShoeShop.Services/Interfaces/IInventoryService.cs
+++ ShoeShop.Services/Interfaces/IInventoryService.cs
@@ -6,20 +6,15 @@ namespace ShoeShop.Services.Interfaces
 {
     public interface IInventoryService
     {
-        // --- Existing Methods ---
         Task<ShoeDto> CreateShoeAsync(CreateShoeDto dto);
         Task<ShoeDto?> GetShoeByIdAsync(int id);
         Task<IEnumerable<ShoeDto>> GetAllShoesAsync();
         Task UpdateShoeAsync(int id, CreateShoeDto dto);
         Task DeleteShoeAsync(int id);
-
         Task UpdateShoeStockAsync(int shoeId, int newStockQuantity);
-
         Task AddColorVariationAsync(int shoeId, int stockQuantity, string colorName, string? hexCode = null);
         Task<IEnumerable<InventoryReportDto>> GetInventoryReportAsync();
         Task<IEnumerable<InventoryReportDto>> GetLowStockAsync();
-
-        // ✅ FIX: Added the method required by the PurchaseOrderController
         Task<IEnumerable<object>> GetAllShoeVariationsAsync();
     }
 }

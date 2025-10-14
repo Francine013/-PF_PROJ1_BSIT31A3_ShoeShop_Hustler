5a0e7b9d3752691b5e7040466edc02ef5260b023
xyrylleclaire-matiga <c2097-23@itmlyceumalabang.onmicrosoft.com>
2025-10-14 13:42:23 +0800

    IDashboardService
    Signed-off-by: xyrylleclaire-matiga <c2097-23@itmlyceumalabang.onmicrosoft.com>

--- ShoeShop.Services/Interfaces/IDashboardService.cs
+++ ShoeShop.Services/Interfaces/IDashboardService.cs
@@ -1,10 +1,11 @@
 ﻿using ShoeShop.Services.DTOs;
 using System.Threading.Tasks;
-
 namespace ShoeShop.Services.Interfaces
 {
     public interface IDashboardService
     {
+        
         Task<DashboardDto> GetDashboardDataAsync();
+        
     }
 }
